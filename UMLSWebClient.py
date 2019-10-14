184e4d826ae77447fa5c54d35888f7a61f89d271
Nima Seyedtalebi <nima.seyedtalebi@uky.edu>
2019-10-14 17:30:54 -0400

    Implemented two important functions today, and kinda tested them a little.

--- UMLSWebClient.py
+++ UMLSWebClient.py
@@ -37,16 +37,75 @@ class UMLSWebClient:
         else:
             self.apikey = environ['UMLS_API_KEY']
         assert(self.apikey is not None)
-        self.tgt = UMLSWebClient._get_tgt()
+        self.tgt = self._get_tgt()
         assert(self.tgt is not None)
 
-    def crosswalk(self, code, source, target):
-        params = self._get_param_dict()
-        params["targetSource"] = str(target)
-        url = UMLSWebClient.API_BASE + "crosswalk/{version}/source/{src}/code/{code}".format(version=self.version,
-                                                                                             src=source,
-                                                                                             code=code,
-                                                                                             )
-        resp = requests.get(url, params=params)
-        resp.raise_for_status()
-        return json.loads(resp.text)
+    '''
+    HEY! The function below is currently BROKEN, but it was almost working.
+    It returns None when it should have results...'''
+    def crosswalk(self, code, source, target=None, page_number=1, page_size=25, page_limit=1):
+        query_params = self._get_param_dict()
+        if target is not None:
+            query_params["targetSource"] = str(target)
+        url = "{base}/crosswalk/{version}/source/{src}/{code}".format(
+            base=UMLSWebClient.API_BASE,
+            version=self.version,
+            src=source,
+            code=code,
+        )
+        results = []
+        cur_page_number = page_number
+        while True:
+            query_params['pageNumber'] = cur_page_number
+            query_params['pageSize'] = page_size
+            r = requests.get(url, params=query_params)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            items = json.loads(r.text)
+            json_data = items["result"]
+            results.extend(json_data)
+            if len(json_data) < page_size:
+                break
+            cur_page_number += 1
+        return results
+
+    def search(self, search_string, input_type="atom", include_obsolete=False, include_suppressible=False,
+                      returnIdType="concept", sabs=None, search_type="words",
+                      page_number=1, page_size=25, page_limit=1):
+        url = self.API_BASE + "/search/" + self.version
+
+        query_params = {
+            'string': search_string,
+            'inputType': input_type,
+            'returnIdType': returnIdType,
+            'searchType': search_type,
+        }
+        if sabs is not None:
+            query_params['sabs'] = sabs
+        query_params['includeObsolete'] = "true" if include_obsolete else "false"
+        query_params['includeSuppressible'] = "true" if include_suppressible else "false"
+        results = []
+        cur_page_number = page_number
+        while True:
+            query_params['ticket'] = self._get_st()
+            query_params['pageNumber'] = cur_page_number
+            query_params['pageSize'] = page_size
+            r = requests.get(url, params=query_params)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            items = json.loads(r.text)
+            json_data = items["result"]
+            results.extend(json_data["results"])
+            if json_data["results"][0]["ui"] == "NONE" or cur_page_number > page_limit:
+                break
+            cur_page_number += 1
+        return results
+
+    def get_cui_from_ui(self, ui):
+        url = "{base}/content/{vers}/CUI/{ui}".format(base=self.API_BASE, vers=self.version, ui=ui)
+        query = self._get_param_dict()
+        r = requests.get(url, params=query)
+        r.encoding = 'utf-8'
+        items = json.loads(r.text)
+        return items["result"]
+
---
+++ test_stuff.py
@@ -0,0 +1,20 @@
+import unittest
+
+import UMLSWebClient as wc
+
+class TestUMLSWebClient(unittest.TestCase):
+    def setUp(self):
+        self.client = wc.UMLSWebClient()
+
+    def test_crosswalk(self):
+        print(self.client.crosswalk("E11.3291", "ICD10CM"))
+
+    #def test_search(self):
+    #    results = self.client.search("broken", page_number=1)
+    #    for result in results:
+    #        print(result['ui']+" "+result["name"])
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUMLSWebClient)
+    unittest.TextTestRunner(verbosity=2).run(suite)
